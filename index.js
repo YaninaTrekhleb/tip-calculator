7e8a241520716de12160fc1188714818b2bbbf77
Yanina Trekhleb <yanisavchenko@gmail.com>
2019-10-21 20:11:28 +0200

    Fixed issue with negative numbers.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function calculateTip() {
-    const billAmount = parseInt(document.getElementById('amountBill').value);
-    const tipPercentage = parseInt(document.getElementById('percentageTip').value);
-    const peolpeNumber = parseInt(document.getElementById('peopleNumber').value);
-    // console.log(typeof billAmount, typeof tipPercentage, typeof peolpeNumber);
+    const amountBillElement = document.getElementById('amountBill');
+    const percentageTipElement = document.getElementById('percentageTip');
+    const peopleNumberElement = document.getElementById('peopleNumber');
+
+    const billAmount = Math.abs(parseInt(amountBillElement.value));
+    const tipPercentage = Math.abs(parseInt(percentageTipElement.value));
+    const peolpeNumber = Math.abs(parseInt(peopleNumberElement.value));
+
+    amountBillElement.value = billAmount;
+    percentageTipElement.value = tipPercentage;
+    peopleNumberElement.value = peolpeNumber;
 
     const totalTip = Math.floor((billAmount * (tipPercentage / 100)));
     const totalToPay = billAmount + totalTip;
@@ -11,6 +18,4 @@ function calculateTip() {
     document.getElementById('tipAmount').innerText = totalTip;
     document.getElementById('totalPay').innerText = totalToPay;
     document.getElementById('perPerson').innerText = tipPerPerson;
-
 }
-
